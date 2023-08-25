29dea5228c407f4c996880d6a37c6d845be1a5f4
kirtany <bhagwandinkirtanya@gmail.com>
2023-08-25 14:55:07 +0200

    new

--- app.js
+++
@@ -1,4 +0,0 @@
-let apiKey = "afd41a03b886cocd08cct4943aae8175";
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Ballito&key=afd41a03b886cocd08cct4943aae8175"; 
-
-console.log(apiUrl)

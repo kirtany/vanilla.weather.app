40e1d56967b3bb99cfb2db4c76277bb518bbe4b9
kirtany <bhagwandinkirtanya@gmail.com>
2023-08-30 21:28:09 +0200

    added timestamp in css

--- src/app.js
+++ src/app.js
@@ -25,32 +25,48 @@ function searchyourCity(event) {
   h1.innerHTML = `${cityInput.value}`;
 
   getWeather(cityInput.value);
-} 
+}  
+ 
+function formatDay(timestamp) { 
+let date = new Date(timestamp * 1000); 
+let day = date.getDay();
+let days = ["Sun","Mon","Tue","Wed","Thur","Fir","Sat"];
+
+return days[day] ; 
+}
+
+
+
+
 function displayForecast(response){
-  console.log(response.data.daily);
+ let forecast= response.data.daily; 
+
   let forecastElement = document.querySelector("#forecast"); 
 
   let days= ["Thur","Fir","Sat","Sun"]; 
 
   let forecastHTML = `<div class ="row">`; 
- days.forEach(function (day) {
+ forecast.forEach(function (forecastDay,index) { 
+  if (index< 6){
   forecastHTML=
   forecastHTML +  
   `
  <div class="col-2"> 
-     <div class="weather-forecast-date">${day}</div> 
+     <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div> 
+    ${index}
      <img 
-     src="http://openweathermap.org/img/wn/50d@2x.png"
+     src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
      alt=""
      width="42"
      /> 
      <div class="weather-forecast-temperature">
-         <span class="weather-forecast-temperature-max">18&deg;C</span> 
-         <span class="weather-forecast-temperature-min">12&deg;C</span> 
+         <span class="weather-forecast-temperature-max">${ Math.round(forecastDay.temp.max)}&deg;C</span> 
+         <span class="weather-forecast-temperature-min">${ Math.round(forecastDay.temp.min)}&deg;C</span> 
      </div>
  </div>
   
  `; 
+  }
 });
   forecastHTML = forecastHTML+ `</div>`;
   forecastElement.innerHTML= forecastHTML; 
